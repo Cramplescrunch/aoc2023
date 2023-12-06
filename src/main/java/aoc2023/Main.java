638ebe073647955030743631fa3c7702ec89f2f2
Arthur Renard <renarda57@gmail.com>
2023-12-06 22:33:40 +0100

    AOC23 - Day1 - First puzzle

--- src/main/java/aoc2023/Main.java
+++ src/main/java/aoc2023/Main.java
@@ -1,10 +1,21 @@
-package src.main.java.aoc2023;
+package aoc2023;
+
+import aoc2023.day1.Day1;
+
+import java.io.File;
+
 /**
  * Main
  */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        try {
+            Day1.printSumOfCalibrationValues(
+                new File("/home/arenard/personal/aoc2023/src/main/resources/d1_input.txt"));
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        
     }
 }
--- src/main/java/aoc2023/day1/Day1.java
+++ src/main/java/aoc2023/day1/Day1.java
@@ -1,4 +1,10 @@
-package src.main.java.aoc2023.day1;
+package aoc2023.day1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class Day1 {
     
@@ -8,20 +14,36 @@ public class Day1 {
         char first=' ';
         char last=' ';
         do {
-            if(Character.isDigit(line[i])) {
+            if(first == ' ' && Character.isDigit(line[i])) {
                 first = line[i];
-            } else {
+            } else if (first == ' ') {
                 i++;
             }
-            if(Character.isDigit(line[j])) {
+            if(last == ' ' && Character.isDigit(line[j])) {
                 last = line[j];
-            } else {
+            } else if (last == ' ') {
                 j--;
             }
             if(first != ' ' && last != ' ') {
                 return Integer.parseInt("" + first + last);
             }
         } while(i < j);
-        return Integer.parseInt("" + line[i] + line[j]);
+        try {
+            return Integer.parseInt("" + line[i] + line[j]);
+        } catch(NumberFormatException e) {
+            System.err.println(e);
+            return 0;
+        }
+    }
+
+    public static void printSumOfCalibrationValues(final File file) throws FileNotFoundException, IOException {
+        try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            long sum=0;
+            while((line = br.readLine()) != null) {
+                sum += Day1.getLineCalibrationValue(line.toCharArray());
+            }
+            System.out.println(sum);
+        }
     }
 }
