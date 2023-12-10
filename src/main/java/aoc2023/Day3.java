e25bfccfe3f973500c2bc54af862e57a7e259039
Arthur Renard <renarda57@gmail.com>
2023-12-10 21:03:15 +0100

    AOC23 - Day3 - Init second puzzle

--- src/main/java/aoc2023/Day3.java
+++ src/main/java/aoc2023/Day3.java
@@ -5,6 +5,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Day3 {
 
@@ -70,6 +73,78 @@ public class Day3 {
             }
         }
         return sum;
+    }
+
+    private static List<Integer[]> getRelatedGears(final int lineNumber, final String line, final String[] currentLines) {
+        
+    }
+
+    private static  Map<Integer[], String[]> updateGearMapForLine(final  Map<Integer[], String[]> map,
+                                                                    final String line,
+                                                                    final String[] currentLines,
+                                                                    final int lineNumber) {
+        Map<Integer[], String[]> updatedMap = map;                                                                
+        String tempNum = "";
+        int sum = 0;
+        int numStart = -1;
+        for (int i=0; i < line.length(); i++) {
+            if(Character.isDigit(line.charAt(i))) {
+                if(numStart == -1) {
+                    numStart = i;
+                }
+                tempNum += line.charAt(i);
+            } else if (tempNum.length() > 0) {
+                if(isPartNumber(numStart, numStart + tempNum.length()-1, currentLines)) {
+                    System.out.println(tempNum);
+                    sum += Integer.parseInt(tempNum);
+                }
+                numStart=-1;
+                tempNum="";
+            }
+        }
+        return updatedMap;
+    }
+
+    private static long getSumOfGearRatiosForGearMap(final  Map<Integer[], String[]> map) {
+        long result = 0;
+        for(String[] value : map.values()) {
+            if(value.length == 2) {
+                result += Integer.parseInt(value[1]) * Integer.parseInt(value[2]);
+            }
+        }
+        return result;
+    }
+
+    public static void printSumOfGearRatios(final File file) throws FileNotFoundException, IOException {
+        String[]currentLines = {"", "", ""};
+        int lineNumber = 0;
+        Map<Integer[], String[]> gearMap = new HashMap<Integer[], String[]>();
+        
+        try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String readLine;
+            // Init current lines
+            currentLines[0] = "";
+            currentLines[1] = br.readLine();
+            currentLines[2] = br.readLine();
+            long sum=0;
+
+            while((readLine = br.readLine()) != null) {
+                gearMap = Day3.updateGearMapForLine(gearMap, currentLines[1], currentLines, lineNumber);
+                // move to next lines 
+                currentLines[0] = currentLines[1];
+                currentLines[1] = currentLines[2];
+                currentLines[2] = readLine;
+                lineNumber++;
+            }
+            // When readLine returns null we must complete the last cycle
+            gearMap = Day3.updateGearMapForLine(gearMap, currentLines[1], currentLines, lineNumber);
+            currentLines[0] = currentLines[1];
+            currentLines[1] = currentLines[2];
+            currentLines[2] = "";
+            lineNumber++;
+            gearMap = Day3.updateGearMapForLine(gearMap, currentLines[1], currentLines, lineNumber);
+            System.out.println(sum);
+        }
     }
 
     public static void printSumPartsNumbers(final File file) throws FileNotFoundException, IOException {
